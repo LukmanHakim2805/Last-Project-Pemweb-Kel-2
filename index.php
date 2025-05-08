451d3173d89ab62c41d2d63942e34d5bcbc16a83
brobilly <billyramadhani04@gmail.com>
2025-05-08 16:43:03 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Halaman Bootstrap Sederhana</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">MySite</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Tentang</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Kontak</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container mt-5">
+    <h1 class="mb-3">Selamat Datang di Toko Kelontong Jaya Jaya Jaya</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure reiciendis quaerat odit distinctio totam voluptatum sed? Sed cupiditate itaque architecto quos. Totam tenetur commodi at cumque enim deleniti iusto dolore.</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptatum sit officia recusandae ratione. Error incidunt similique animi rerum veniam, voluptate reprehenderit quia obcaecati, odit commodi deleniti, inventore dolorem quisquam.</p>
+
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis ab numquam vitae odit, error qui a eligendi eaque quis, officiis quidem tenetur! Sed voluptate omnis enim sint, pariatur animi perspiciatis?</p>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
